92c1d5b31a85d39bd680135ff5fcff1011e4e9eb
ezetabo <ezetabo78@gmail.com>
2023-08-31 17:08:30 -0300

    fin de aplicacion

---
+++ src/app/gifs/componets/gifs-card/gifs-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interface';
+
+@Component({
+  selector: 'gifs-gifs-card',
+  templateUrl: './gifs-card.component.html',
+  styleUrls: ['./gifs-card.component.css']
+})
+export class GifsCardComponent implements OnInit {
+
+  @Input()
+  public gif!: Gif;
+
+
+  ngOnInit(): void {
+    if (!this.gif) {
+      throw new Error('Gifs porperty is required');
+    }
+  }
+}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
+import { ShareModule } from '../share/share.module';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SerchBoxComponent } from './componets/serch-box/serch-box-componet';
 import { CardListComponent } from './componets/card-list/card-list.component';
+import { GifsCardComponent } from './componets/gifs-card/gifs-card.component';
 
 
 
 @NgModule({
   declarations: [
+    CardListComponent,
+    GifsCardComponent,
     HomePageComponent,
     SerchBoxComponent,
-    CardListComponent
   ],
   imports: [
     CommonModule,
+    ShareModule
   ],
   exports: [
     HomePageComponent
